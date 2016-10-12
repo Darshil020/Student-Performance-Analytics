3e928ee7d2a0d33ee077323849be524edf572da9
sanket <gets.sanket.patel@gmail.com>
2016-10-11 22:20:13 -0500

    updating correlation

--- preProcessing.r
+++ preProcessing.r
@@ -8,23 +8,5 @@ count(Maths,"Result")
 
 #Correlation of G1 with G3
 
-library(dplyr)
-library(reshape2)
-
-setwd("C:/Users/akash/Documents/GitHub/StudentPerformancePrediction")
-Maths=read.table("student-mat.csv",sep=";",header=TRUE)
-
-d1 <- data.frame(Maths$G1, Maths$G3)
-d1_cor <- as.matrix(cor(d1))
-d1_cor_melt <- arrange(melt(d1_cor), -abs(value))
-d1_cor_melt
-
-#Correlation of G2 with G3
-
-setwd("C:/Users/akash/Documents/GitHub/StudentPerformancePrediction")
-Maths=read.table("student-mat.csv",sep=";",header=TRUE)
-
-d2 <- data.frame(Maths$G2, Maths$G3)
-d2_cor <- as.matrix(cor(d2))
-d2_cor_melt <- arrange(melt(d2_cor), -abs(value))
-d2_cor_melt
+G <- data.frame(Maths$G1,Maths$G2 ,Maths$G3)
+correlation <- as.matrix(cor(G))
