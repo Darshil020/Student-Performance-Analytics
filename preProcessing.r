ba36dea3ccdfb0815b0cb60ff7f6a85dc2ed90a1
akashvjoshi <akashvjoshi@gmail.com>
2016-10-12 00:11:10 -0500

    Added Boxplot query

--- preProcessing.r
+++ preProcessing.r
@@ -28,3 +28,20 @@ d2 <- data.frame(Maths$G2, Maths$G3)
 d2_cor <- as.matrix(cor(d2))
 d2_cor_melt <- arrange(melt(d2_cor), -abs(value))
 d2_cor_melt
+
+
+#Correlation of G3- Maths and G3-Por
+setwd("C:/Users/akash/Documents/GitHub/StudentPerformancePrediction")
+Maths=read.table("student-mat.csv",sep=";",header=TRUE)
+Por=read.table("student-por.csv",sep=";",header=TRUE)
+
+MP <- data.frame(Maths$G3, Por$G3)
+MP_cor <- as.matrix(cor(MP))
+MP_cor_melt <- arrange(melt(d2_cor), -abs(value))
+MP_cor_melt
+
+#Boxplots
+
+par(mfrow=c(1,2))
+boxplot( Maths$G3, data = Maths,col="yellow",main = "Maths", ylab = "G3 Score")
+boxplot(Por$G3, data = Por,col = "green", main = "Portugese", ylab = "G3 Score")
