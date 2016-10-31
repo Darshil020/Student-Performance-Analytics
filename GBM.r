3e175297e0dfa6a81ce7f390db8406c3dd0f9efe
sanket <gets.sanket.patel@gmail.com>
2016-10-31 11:56:16 -0500

    updated the files
    added pruning and improved accuracy.

--- GBM.r
+++ GBM.r
@@ -8,8 +8,6 @@ nested_ifelse <- function(x)
   ifelse (x < 10,"fail",ifelse(x < 12, "D", ifelse(x < 14,"C",ifelse(x < 16, "B","A")) ))
 Result <- nested_ifelse(Maths$G3)  #converting G3 to categorical Result
 
-
-
 Maths <- data.frame(Maths,Result) #appending the grades Result
 Maths = Maths[,-33]        #remove the G3
 Maths = Maths[,-30]
@@ -24,7 +22,7 @@ testing_data = Maths[test,]
 testing_result = Result[test]
 
 #GBM
-fit<- gbm(Result~.,data=Maths,distribution = "multinomial",n.trees = 500,shrinkage = 0.05,interaction.depth = 3,bag.fraction = 0.5,keep.data = FALSE,cv.folds = 5)
+fit<- gbm(Result~.,data=training_data,distribution = "multinomial",n.trees = 500,shrinkage = 0.05,interaction.depth = 3,bag.fraction = 0.5,keep.data = FALSE,cv.folds = 5)
 nTrees <- gbm.perf(fit)
 print(nTrees)
 print(summary(fit))
@@ -40,10 +38,11 @@ for(colname in colnames(test_answer))({
   coldata=test_answer[,colname]
   test_answer$Answer[which(coldata==1)]=colname
 })
-test_answer$Answer<-gsub(".62","",test_answer$Answer)
+test_answer$Answer<-gsub(".40","",test_answer$Answer)
 test_answer$Answer<- as.factor(test_answer$Answer)
 #mean(test_answer$Answer == testing_result,na.rm = FALSE)
 table(test_answer$Answer,testing_result) #88.38% 15,29,27,41,63
+
 
 
 
