99923531568e2b78302feb3ca8cf616f2fe4337e
hetulp <patelhetulk@gmail.com>
2016-11-23 17:12:14 -0600

    latest gbm

--- GBM_Grades.R
+++ GBM_Grades.R
@@ -1,12 +1,12 @@
 library(gbm)
-setwd("C:/Users/akash/Documents/GitHub/StudentPerformancePrediction")
+setwd("C:/Users/User/Documents/GitHub/StudentPerformancePrediction")
 Maths=read.table("student-mat.csv",sep=";",header=TRUE)
 head(Maths)
 
 #converting the result into catagorical
 
 nested_ifelse <- function(x) 
-  ifelse (x < 10,"fail",ifelse(x < 12, "D", ifelse(x < 14,"C",ifelse(x < 16, "B","A")) ))
+  ifelse (x < 10,0,ifelse(x < 12, 1, ifelse(x < 14,2,ifelse(x < 16, 3,4)) ))
 
 Result <- nested_ifelse(Maths$G3)  #converting G3 to categorical Result
 Result
@@ -24,15 +24,19 @@ testing_data = Maths[test,]
 testing_result = Result[test]
 
 #GBM
-fit<- gbm(Result~.,data=training_data,distribution = "multinomial",n.trees = 500,shrinkage = 0.05,interaction.depth = 3,bag.fraction = 0.5,keep.data = FALSE,cv.folds = 5)
+fit<- gbm(Result~.,data=training_data,distribution = "multinomial",n.trees = 5000,shrinkage = 0.01,interaction.depth = 1,bag.fraction = 0.5,keep.data = FALSE,cv.folds = 5)
 nTrees <- gbm.perf(fit)
 print(nTrees)
 print(summary(fit))
 
 test_prediction <- predict(fit,testing_data,n.trees = nTrees,type = "response")
-test_answer <- round(test_prediction)
+test_prediction <- as.data.frame(test_prediction)
 
-test_answer<-as.data.frame(test_answer)
+test_answer <- t(apply(test_prediction, 1, function(z){ 
+  1 * (z == max(z)) 
+})) 
+test_answer <- as.data.frame(test_answer)
+
 
 test_answer$Answer=NA
 colnames(test_answer)
@@ -40,7 +44,8 @@ for(colname in colnames(test_answer))({
   coldata=test_answer[,colname]
   test_answer$Answer[which(coldata==1)]=colname
 })
-test_answer$Answer<-gsub(".41","",test_answer$Answer)
+test_answer$Answer<-gsub(".350","",test_answer$Answer)
 test_answer$Answer<- as.factor(test_answer$Answer)
 mean(test_answer$Answer == testing_result,na.rm = FALSE)
 table(test_answer$Answer,testing_result) #887.38% 15,29,27,41,63
+sum(is.na(test_answer$Answer))
